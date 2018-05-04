0e439bdf0a36d6754b5835b88c00610daa000042
Andrii Rosa <andriirosa@fb.com>
2018-05-04 17:01:46 -0400

    Add tests for interpreted IN on complex types

--- presto-main/src/test/java/com/facebook/presto/sql/TestExpressionInterpreter.java
+++ presto-main/src/test/java/com/facebook/presto/sql/TestExpressionInterpreter.java
@@ -449,6 +449,56 @@ public class TestExpressionInterpreter
     }
 
     @Test
+    public void testInComplexTypes()
+    {
+        assertOptimizedEquals("ARRAY[1] IN (ARRAY[1])", "true");
+        assertOptimizedEquals("ARRAY[1] IN (ARRAY[2])", "false");
+        assertOptimizedEquals("ARRAY[1] IN (ARRAY[2], ARRAY[1])", "true");
+//        assertOptimizedEquals("ARRAY[1] IN (null)", "null");
+        assertOptimizedEquals("ARRAY[1] IN (null, ARRAY[1])", "true");
+        assertOptimizedEquals("ARRAY[1, 2, null] IN (ARRAY[2, null], ARRAY[1, null])", "false");
+//        assertOptimizedEquals("ARRAY[1, null] IN (ARRAY[2, null], null)", "false");
+//        assertOptimizedEquals("ARRAY[null] IN (ARRAY[null])", "null");
+//        assertOptimizedEquals("ARRAY[1] IN (ARRAY[null])", "null");
+//        assertOptimizedEquals("ARRAY[null] IN (ARRAY[1])", "null");
+//        assertOptimizedEquals("ARRAY[1, null] IN (ARRAY[1, null])", "null");
+//        assertOptimizedEquals("ARRAY[1, null] IN (ARRAY[2, null])", "false");
+//        assertOptimizedEquals("ARRAY[1, null] IN (ARRAY[1, null], ARRAY[2, null])", "false");
+//        assertOptimizedEquals("ARRAY[1, null] IN (ARRAY[1, null], ARRAY[2, null], ARRAY[1, null])", "false");
+
+        assertOptimizedEquals("ROW(1) IN (ROW(1))", "true");
+        assertOptimizedEquals("ROW(1) IN (ROW(2))", "false");
+        assertOptimizedEquals("ROW(1) IN (ROW(2), ROW(1), ROW(2))", "true");
+//        assertOptimizedEquals("ROW(1) IN (null)", "null");
+//        assertOptimizedEquals("ROW(1) IN (null, ROW(1))", "true");
+//        assertOptimizedEquals("ROW(1, null) IN (ROW(2, null), null)", "false");
+//        assertOptimizedEquals("ROW(null) IN (ROW(null))", "null");
+//        assertOptimizedEquals("ROW(1) IN (ROW(null))", "null");
+//        assertOptimizedEquals("ROW(null) IN (ROW(1))", "null");
+//        assertOptimizedEquals("ROW(1, null) IN (ROW(1, null))", "null");
+        assertOptimizedEquals("ROW(1, null) IN (ROW(2, null))", "false");
+//        assertOptimizedEquals("ROW(1, null) IN (ROW(1, null), ROW(2, null))", "false");
+//        assertOptimizedEquals("ROW(1, null) IN (ROW(1, null), ROW(2, null), ROW(1, null))", "false");
+
+
+        assertOptimizedEquals("MAP(ARRAY[1], ARRAY[1]) IN (MAP(ARRAY[1], ARRAY[1]))", "true");
+//        assertOptimizedEquals("MAP(ARRAY[1], ARRAY[1]) IN (null)", "null");
+        assertOptimizedEquals("MAP(ARRAY[1], ARRAY[1]) IN (null, MAP(ARRAY[1], ARRAY[1]))", "true");
+        assertOptimizedEquals("MAP(ARRAY[1], ARRAY[1]) IN (MAP(ARRAY[1, 2], ARRAY[1, null]))", "false");
+//        assertOptimizedEquals("MAP(ARRAY[1, 2], ARRAY[1, null]) IN (MAP(ARRAY[1, 2], ARRAY[2, null]), null)", "false");
+//        assertOptimizedEquals("MAP(ARRAY[1, 2], ARRAY[1, null]) IN (MAP(ARRAY[1, 2], ARRAY[1, null]))", "null");
+//        assertOptimizedEquals("MAP(ARRAY[1, 2], ARRAY[1, null]) IN (MAP(ARRAY[1, 3], ARRAY[1, null]))", "null");
+//        assertOptimizedEquals("MAP(ARRAY[1], ARRAY[null]) IN (MAP(ARRAY[1], ARRAY[null]))", "null");
+//        assertOptimizedEquals("MAP(ARRAY[1], ARRAY[1]) IN (MAP(ARRAY[1], ARRAY[null]))", "null");
+//        assertOptimizedEquals("MAP(ARRAY[1], ARRAY[null]) IN (MAP(ARRAY[1], ARRAY[1]))", "null");
+//        assertOptimizedEquals("MAP(ARRAY[1, 2], ARRAY[1, null]) IN (MAP(ARRAY[1, 2], ARRAY[1, null]))", "null");
+        assertOptimizedEquals("MAP(ARRAY[1, 2], ARRAY[1, null]) IN (MAP(ARRAY[2, 2], ARRAY[1, null]))", "false");
+        assertOptimizedEquals("MAP(ARRAY[1, 2], ARRAY[1, null]) IN (MAP(ARRAY[1, 2], ARRAY[2, null]))", "false");
+//        assertOptimizedEquals("MAP(ARRAY[1, 2], ARRAY[1, null]) IN (MAP(ARRAY[1, 2], ARRAY[1, null]), MAP(ARRAY[1], ARRAY[2, null]))", "false");
+//        assertOptimizedEquals("MAP(ARRAY[1, 2], ARRAY[1, null]) IN (MAP(ARRAY[1, 2], ARRAY[1, null]), MAP(ARRAY[1], ARRAY[2, null]), MAP(ARRAY[1], ARRAY[1, null]))", "false");
+    }
+
+    @Test
     public void testCurrentTimestamp()
     {
         double current = TEST_SESSION.getStartTime() / 1000.0;
